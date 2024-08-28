dd903742f0557fb8183ac6d1690bd78041701c57
Marqzzs <maumarqz007@gmail.com>
2024-08-28 18:02:08 -0300

    fazendo

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,16 +4,21 @@ import memojiImage from '@/assets/images/memoji-computer.png';
 import Image from 'next/image';
 import ArrowDow from '@/assets/icons/arrow-down.svg';
 import grainImage from '@/assets/images/grain.jpg';
+import StarIcon from '@/assets/icons/star.svg'
 
 export const HeroSection = () => {
 	return (
-		<div className="py-32 md:py-48 lg:py-60 relative">
+		<div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
 			<div
 				className="absolute inset-0 -z-30 opacity-5"
 				style={{
 					backgroundImage: `url(${grainImage.src})`,
 				}}
 			></div>
+			<div className="size-[620px] hero-ring"></div>
+			<div className="size-[820px] hero-ring"></div>
+			<div className="size-[1020px] hero-ring"></div>
+			<div className="size-[1220px] hero-ring"></div>
 			<div className="container">
 				<div className="flex flex-col items-center">
 					<Image
